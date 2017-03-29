3ff19aeb3086006ff197fd4f2ee6bf15fa7157e8
Matt0911 <Mattmanhardt@Gmail.com>
2017-03-28 18:23:35 -0600

    this time actually copy lab 5 as a start

--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,58 @@
 var express = require('express');
-var nodemailer = require('nodemailer');
 var router = express.Router();
+/* Set up mongoose in order to connect to mongo database */
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+mongoose.connect('mongodb://localhost/commentDB'); //Connects to a mongo database called "commentDB"
+
+var commentSchema = mongoose.Schema({ //Defines the Schema for this database
+  Name: String,
+  Comment: String
 });
 
-router.post('/sayHello', function (req, res) {
-  console.log(req.body);
+var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
 
-  // var helper = require('sendgrid').mail;
-  // var from_email = new helper.Email(req.body.sender);
-  // var to_email = new helper.Email(req.body.receiver);
-  // var subject = req.body.subject;
-  // var content = new helper.Content('text/plain', req.body.message);
-  // var mail = new helper.Mail(from_email, subject, to_email, content);
-  //
-  // var sg = require('sendgrid')('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');
-  // var request = sg.emptyRequest({
-  //   method: 'POST',
-  //   path: '/v3/mail/send',
-  //   body: mail.toJSON(),
-  // });
-  //
-  // sg.API(request, function(error, response) {
-  //   console.log(response.statusCode);
-  //   console.log(response.body);
-  //   console.log(response.headers);
-  // });
-  // var options = {
-  //   auth: {
-  //     api_user: 'creative4email',
-  //     api_key: 'creative4'
-  //   }
-  // }
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'creative4email@gmail.com', // Your email id
-            pass: 'creative4' // Your password
-        }
-    });
-
-    // var transporter = nodemailer.createTransport({
-    //   service: 'SendGrid',
-    //   auth: {
-    //     user: 'creative4email',
-    //     pass: 'creative4'
-    //   }
-    // });
-
-    var text = 'Hello from \n\n';
-
-    var mailOptions = {
-      from: req.body.sender, // sender address
-      to: req.body.receiver, // list of receivers
-      subject: req.body.subject, // Subject line
-      text: req.body.message //, // plaintext body
-      // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          console.log(error);
-          res.send("There was an error");
-      }else{
-          console.log('Message sent: ' + info.response);
-          res.send("Email was sent!");
-      };
-    });
-
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+  console.log('Connected');
 });
+
+
+router.post('/comment', function(req, res, next) {
+  console.log("POST comment route"); //[1]
+  console.log(req.body); //[2]
+  var newcomment = new Comment(req.body); //[3]
+  console.log(newcomment); //[3]
+  newcomment.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+});
+
+/* GET comments from database */
+router.get('/comment', function(req, res, next) {
+  console.log("In the GET route?");
+  Comment.find(function(err,commentList) { //Calls the find() method on your database
+    if (err) return console.error(err); //If there's an error, print it out
+    else {
+      console.log(commentList); //Otherwise console log the comments you found
+      res.json(commentList);
+
+    }
+  })
+});
+
+router.delete('/comment', function(req, res, next) {
+  console.log('Delete route');
+  Comment.remove({}, function(err){
+    if (err) return console.error(err);
+    else {
+      console.log('deleted comments');
+      res.sendStatus(200);
+    }
+  });
+})
 
 module.exports = router;
