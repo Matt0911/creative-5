debd353bb4e984da8988440263e7b2afdfb19dc9
Joseph Hansen <josephmhansen@gmail.com>
2017-03-30 04:36:25 +0000

    more mapStuff js and new elements in index html. Now just need to add 16 map images of just a single dot to show when a temple is highlighted

--- public/javascripts/mapStuff.js
+++ public/javascripts/mapStuff.js
@@ -1,7 +1,32 @@
 function checkSelected() {
-  if $(
+  var ids = [
+    'bountiful',
+    'brigham-city',
+    'draper',
+    'jordan-river',
+    'logan',
+    'manti',
+    'monticello',
+    'mount-timpanogos',
+    'ogden',
+    'oquirrh-mountain',
+    'payson',
+    'provo-city-center',
+    'provo',
+    'st-george',
+    'salt-lake',
+    'vernal'
+  ]
+  for (var i = 0, len = ids.length; i < len; i++) {
+    if ($('#'+ids[i]).prop('checked')) {
+      $('#'+ids[i]+'-map').show()
+    } else {
+      $('#'+ids[i]+'-map').hide()
+    }
+  }
 }
 
 $(document).ready(function() {
   checkSelected()
+  $('input').click(checkSelected)
 })
